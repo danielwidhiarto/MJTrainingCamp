df50102c932ca26b4573c79f72c9268336c6c517
danielwidhiarto <danielwidhiarto@gmail.com>
2024-10-17 12:25:11 +0700

    add routing for member, admin. and trainer

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../components/Login.vue' // Import Login component
-import Register from '../components/Register.vue' // Import Register component
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
+import MemberDashboard from '../components/Member/Home.vue'
+import AdminDashboard from '../components/Admin/Dashboard.vue'
+import TrainerDashboard from '../components/Trainer/Dashboard.vue'
 
 const routes = [
   {
@@ -13,11 +16,46 @@ const routes = [
     name: 'Register',
     component: Register,
   },
+  {
+    path: '/member-dashboard',
+    name: 'MemberDashboard',
+    component: MemberDashboard,
+  },
+  {
+    path: '/admin-dashboard',
+    name: 'AdminDashboard',
+    component: AdminDashboard,
+  },
+  {
+    path: '/trainer-dashboard',
+    name: 'TrainerDashboard',
+    component: TrainerDashboard,
+  },
 ]
 
+// Create the router instance
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+// Navigation guard to protect routes
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  const role = localStorage.getItem('role')
+
+  if (to.name === 'AdminDashboard' && role !== 'ROLE_ADMIN') {
+    // If trying to access Admin Dashboard and user is not admin, redirect to User Home
+    next({ name: 'MemberDashboard' })
+  } else if (to.name === 'TrainerDashboard' && role !== 'ROLE_TRAINER') {
+    // If trying to access Trainer Dashboard and user is not a trainer, redirect to User Home
+    next({ name: 'MemberDashboard' })
+  } else if (to.name !== 'Login' && !token) {
+    // If trying to access a protected route and not logged in, redirect to Login
+    next({ name: 'Login' })
+  } else {
+    next() // Allow access to the route
+  }
 })
 
 export default router
