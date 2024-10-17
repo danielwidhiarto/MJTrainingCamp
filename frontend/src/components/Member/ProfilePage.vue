9b87ddaf98dd00a482bcf23c845bcd338a70f721
danielwidhiarto <danielwidhiarto@gmail.com>
2024-10-17 15:09:49 +0700

    add profile page

---
+++ frontend/src/components/Member/ProfilePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <h1>ProfilePage</h1>
+      <p>Welcome to your ProfilePage page!</p>
+      <!-- Add more content specific to the user here -->
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../Navbar.vue' // Import the Navbar component
+
+export default {
+  name: 'ProfilePage',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style>
+.container {
+  padding: 20px;
+}
+</style>
